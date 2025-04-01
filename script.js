99ceee4763d6cbe5a92eefa875402b85b3d5de99
csmanhaes86 <charlesmanhaes1@gmail.con>
2025-04-01 01:39:12 -0300

    configurando o botão e formulario no javascript

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-console.log("Funcionou");
+const form = document.querySelector(".formulario-orcamento");
+const mascara = document.querySelector(".mascara-formulario");
 
+function mostrarForm() {
+  form.style.left = "50%";
+  form.style.transform = "translateX(-50%)";
+  mascara.style.visibility = "visible";
+}
+
+function esconderForm() {
+  form.style.left = "-300px";
+  form.style.transform = "translateX(0)";
+  mascara.style.visibility = "hidden";
+}
